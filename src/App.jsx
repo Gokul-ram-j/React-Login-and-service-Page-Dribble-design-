36b70e0bd48910cf6937c6f9dd4dfcbc72245f02
gokul-ram-j <gokulram221@gmail.com>
2024-04-09 18:10:49 +0530

    nt1

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import Form from "./components/Form";
 import Profile from "./components/Profile";
 import Service from "./components/Service";
 import Verification from "./components/Verification";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Routes>
-          <Route path="/" element={<Form/>}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Form />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/service" element={<Service />} />
           <Route path="/verification" element={<Verification />} />
-      </Routes>
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </>
   );
