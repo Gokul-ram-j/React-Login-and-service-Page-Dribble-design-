e00f6d543058cc452ddd7c2ab57b0389fceccea7
gokul-ram-j <gokulram221@gmail.com>
2024-04-09 17:50:52 +0530

    normal

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://gokul-ram-j.github.io/Aeonaxy-Technologies-Private-Limited/",
   plugins: [react()],
 })
